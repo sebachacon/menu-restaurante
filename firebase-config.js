e5bc2d92ce12e9993707b614749ca6a15502179b
OskrVillVar <Alviva379000@gmail.com>
2026-05-07 10:51:16 -0600

    Agregar categorías del menú

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey:            "AIzaSyCGuhLS_JwR0dCcIGmPQOERprT9cpNPsNc",
+  authDomain:        "menu-restaurante-ctp.firebaseapp.com",
+  databaseURL:       "https://menu-restaurante-ctp-default-rtdb.firebaseio.com",
+  projectId:         "menu-restaurante-ctp",
+  storageBucket:     "menu-restaurante-ctp.firebasestorage.app",
+  messagingSenderId: "96866323208",
+  appId:             "1:96866323208:web:79aecfafc2def5c7e4fabd"
+};
